d5dcfb40bd35907cc7f052ccfc25646c82cb1236
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2022-03-16 18:29:12 -0300

    minor bug fixes

--- jammming/src/Components/SearchBar/SearchBar.js
+++ jammming/src/Components/SearchBar/SearchBar.js
@@ -24,14 +24,18 @@ class SearchBar extends React.Component {
 
     handleKeyPress(event) {
         if (event.key ==='Enter') {
-            this.props.onSearch(this.state.term);
+            this.search();
         }
     }
     
     render() {
         return (
             <div className="SearchBar">
-                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" onKeyPress={this.handleKeyPress}/>
+                <input 
+                    onChange={this.handleTermChange} 
+                    placeholder="Enter A Song, Album, or Artist" 
+                    onKeyPress={this.handleKeyPress}
+                />
                 <button onClick={this.search} className="SearchButton" >SEARCH</button>
             </div>
         );
